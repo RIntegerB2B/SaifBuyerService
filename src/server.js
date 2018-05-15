9565441f3a5d163fc3d8e1cba553fb17f2c8c2ae
Chandran <Preveen_Chandran@Dell.com>
2018-05-15 19:58:51 +0530

    Formatted

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3000,
-  bodyParser = require('body-parser');
+    app = express(),
+    port = process.env.PORT || 3000,
+    bodyParser = require('body-parser');
 var cors = require('cors');
 var exec = require('child_process').exec;
 
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 app.listen(port);
 
@@ -17,31 +17,47 @@ var mongoose = require('mongoose');
 mongoose.connect(mongoDbConfig.url, {
 });
 
-mongoose.connection.on('error', function() {
+mongoose.connection.on('error', function () {
     console.log('Could not connect to the database. Exiting now...');
     process.exit();
 });
 
-mongoose.connection.once('open', function() {
+mongoose.connection.once('open', function () {
     console.log("Successfully connected to the database");
 })
 
 app.get('/test', function (req, res) {
-  res.end( "Success" );
+    res.end("Success");
 })
 
-app.get('/deploy', function (req, res) {
- 
-var yourscript = exec('sh /home/ubuntu/myfolder/batchfiles/deploy-buyer-app.sh',
+app.get('/deployApp', function (req, res) {
+
+    var yourscript = exec('sh /home/ubuntu/myfolder/batchfiles/deploy-buyer-app.sh',
         (error, stdout, stderr) => {
-            console.log(`${stdout}`);
+            // console.log(`${stdout}`);
+            res.end(`${stdout}`);
             console.log(`${stderr}`);
             if (error !== null) {
                 console.log(`exec error: ${error}`);
             }
         });
-  res.end( "Success" );
-  yourscript();
+
+    yourscript();
+})
+
+app.get('/deployService', function (req, res) {
+
+    var yourscript = exec('sh /home/ubuntu/myfolder/batchfiles/deploy-buyer-service.sh',
+        (error, stdout, stderr) => {
+            // console.log(`${stdout}`);
+            res.end(`${stdout}`);
+            console.log(`${stderr}`);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+        });
+
+    yourscript();
 })
 
 
