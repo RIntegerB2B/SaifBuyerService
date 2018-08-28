df028575f0e268cdc61d9db83e2fdc344c31fdfa
Chandran <Preveen_Chandran@Dell.com>
2018-08-28 15:09:51 +0530

    ENV Variable

--- src/config/appSetting.js
+++ src/config/appSetting.js
@@ -1,5 +1,5 @@
 module.exports = {
-    imageServerPath: 'http://localhost/Seller/'
+    imageServerPath: process.env.BUYER_IMAGE_SERVER_PATH || 'http://localhost/Seller/'
   //  imageServerPath: 'http://ec2-13-126-16-163.ap-south-1.compute.amazonaws.com:81/images/'
 
 }
