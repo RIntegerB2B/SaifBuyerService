2e686476b4c6b67973df2a934162b9fd23fb5bba
ripsildeveloper <ripsildeveloper3@gmail.com>
2019-03-05 18:13:12 +0530

    changes in settings

---
+++ src/config/appSetting.js
@@ -0,0 +1,9 @@
+module.exports = {
+  productUploadPath: './images/products',
+  productServerPath: 'http://localhost/ArifB2B/products/',
+  bannerUploadPath: './images/banners',
+  bannerServerPath: 'http://localhost/ArifB2B/banners/',
+  adsUploadPath: './images/ads',
+  adsServerPath: 'http://localhost/ArifB2B/ads/',
+  productServerPath: 'http://localhost/ArifB2B/'
+};
