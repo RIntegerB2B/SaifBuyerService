6fa13dc1ff9cbd5630ff664b88542265740092c5
VikasiniN <spl.anand@gmail.com>
2019-05-14 15:06:11 +0530

    changes in cinfig

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 # SAifBuyerService
-=======
-# SaifBuyerService
->>>>>>> 510ff28a0b0a04995772b34f77c910f1ceb0a0db
--- src/config/appSetting.js
+++ src/config/appSetting.js
@@ -1,7 +1,12 @@
 module.exports = {
-  productServerPath: 'http://localhost/SaifB2B/products/',
+/*   productServerPath: 'http://localhost/SaifB2B/products/',
   bannerServerPath: 'http://localhost/SaifB2B/banners/',
   adsServerPath: 'http://localhost/SaifB2B/ads/',
   categoryServerPath: 'http://localhost/SaifB2B/category/',
-  logoServerPath: 'http://localhost/SaifB2B/logo/'
+  logoServerPath: 'http://localhost/SaifB2B/logo/' */
+  productServerPath: 'http://sofacreator.com/admin/images/products/',
+  bannerServerPath: 'http://sofacreator.com/admin/images/banners/',
+  adsServerPath: 'http://sofacreator.com/admin/images/ads/',
+  categoryServerPath: 'http://sofacreator.com/admin/images/category/',
+  logoServerPath: 'http://sofacreator.com/admin/images/logo/'
 };
